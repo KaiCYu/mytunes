02f93f421a212886167a9c0595109a576ca2cd49
KaiCYu <kaichyu@gmail.com>
2017-02-20 20:58:01 -0800

    worked on songQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,49 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    //add event listeners
+    this.on('add', this.enqueue, this);
+    this.on('remove', this.playNext, this);
+    this.on('ended', this.dequeue, this);
+
+
+
+    this.on('enqueue', this.enqueue, this);
+    this.on('dequeue', this.deQueue, this);
+
+  },
+
+  enqueue: function() {
+    if (this.models.length === 1) {
+      this.playFirst();
+    }
+  },
+
+  playFirst: function() {
+      this.models[0].play;
+  },
+
+  dequeue: function() {
+    this.shift();
+    if (this.models.length >= 1) {
+      this.playFirst();
+    }
+  },
+
+  playNext: function() {
+    //WARNING
+    if(this.models.length >= 1) {
+      this.models[0].play;
+    }
   }
+
+
+
+  //
+  //
+  // removeFromQueue: function(song) {
+  //   this.remove(song);
+  // },
+
 
 });
